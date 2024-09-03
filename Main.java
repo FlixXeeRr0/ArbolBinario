5830635ff0960a271be452af49e4a7fc98a8b4c8
Agustin Cardoza <137811580+FlixXeeRr0@users.noreply.github.com>
2024-09-02 21:02:22 -0700

    Se modifica clase main para que genere numeros pseudoaleatorios de forma automatica en la insercion de nodos o de forma manual. Tambien se reestructura codigo y se elaboran metodos

--- Main.java
+++ Main.java
@@ -1,45 +1,81 @@
 import java.util.Scanner;
 
 public class Main {
+  public static Scanner leer = new Scanner(System.in);
   public static void main(String[] args) {
-    Scanner leer = new Scanner(System.in);
+    // Declaración del arbol
+    ArbolBinario arbolBinario = new ArbolBinario();
 
-    ArbolBinario abb = new ArbolBinario();
+    // Verificamos si el arbol esta vacio
+    validarVacio(arbolBinario);
 
-    // Verificar si el árbol está vacío
-    if (abb.estaVacio()) {
-      System.out.println("El árbol está vacío.");
-    } else {
-        System.out.println("El árbol no está vacío.");
+    // Solicitamos cantidad de nodos a agregar
+    System.out.print("Ingrese la cantidad de nodos a agregar: ");
+    int cantidadNodos = leer.nextInt();
+    int opcion;
+    do{
+      System.out.print("Ingrese 1 para carga manual o 2 para carga automatica: ");
+      opcion = leer.nextInt();
+    }while(opcion != 1 && opcion != 2);
+    if(opcion == 1){
+      cargaDeNodosManual(arbolBinario, cantidadNodos);
+    }else{
+      cargaDeNodosAutomatica(arbolBinario, cantidadNodos);
     }
 
-    // ELEMENTOS PARA EL ARBOL 43,10,8,54,15,50,53
-    abb.agregar(43);
-    abb.agregar(10);
-    abb.agregar(8);
-    abb.agregar(54);
-    abb.agregar(15);
-    abb.agregar(50);
-    abb.agregar(53);
-
+    // Mostramos los diferentes recorridos del arbol
+    System.out.println("");
     System.out.print("INORDEN: ");
-    abb.recorrerInorden();
+    arbolBinario.recorrerInorden();
     System.out.print("\nPREORDEN: ");
-    abb.recorrerPreorden();
+    arbolBinario.recorrerPreorden();
     System.out.print("\nPOSTORDEN: ");
-    abb.recorrerPostorden();
+    arbolBinario.recorrerPostorden();
     System.out.println("");
 
-    // Buscar un nodo
-    System.out.print("Ingrese el numero del nodo a buscar: ");
+    // Busqueda de algún nodo
+    System.out.print("Ingrese el elemento a buscar: ");
     int valorBuscar = leer.nextInt();
-    Nodo nodoEncontrado = abb.buscarValor(valorBuscar);
-    if (nodoEncontrado != null) {
-        System.out.println("Nodo encontrado.");
-    } else {
-        System.out.println("Nodo no encontrado.");
-    }
+    buscarNodo(arbolBinario, valorBuscar);
 
+
+    // Verificamos nuevamente si el arbol esta vacio
+    validarVacio(arbolBinario);
+    // Cerramos el scanner
     leer.close();
+  }
+
+  public static void validarVacio(ArbolBinario arbol) {
+    if (arbol.estaVacio()) {
+      System.out.println("El arbol esta vacio.");
+    } else {
+      System.out.println("El arbol no esta vacio.");
+    }
+  }
+
+  public static void cargaDeNodosManual(ArbolBinario arbol, int cantidadNodos) {
+    for (int i = 0; i < cantidadNodos; i++) {
+      System.out.println("Ingrese el valor del nodo " + (i + 1) + ": ");
+      int valor = leer.nextInt();
+      arbol.agregar(valor);
+    }
+  }
+
+  public static void cargaDeNodosAutomatica(ArbolBinario arbol, int cantidadNodos) {
+    System.out.print("Carga automatica con valores: ");
+    for (int i = 0; i < cantidadNodos; i++) {
+      int valor = (int) (Math.random() * 100);
+      System.out.print(valor + ", ");
+      arbol.agregar(valor);
+    }
+  }
+
+  public static void buscarNodo(ArbolBinario arbol, int valor) {
+    Nodo nodoEncontrado = arbol.buscarValor(valor);
+    if (nodoEncontrado != null) {
+      System.out.println("Si existe el valor proporcionado dentro del arbol.");
+    } else {
+      System.out.println("No existe el valor proporcionado en el arbol.");
+    }
   }
 }
